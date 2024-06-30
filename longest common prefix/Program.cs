c28b2eb44193be244eb7cf375550fc10c49b62af
StevenLo0528 <a131835258@gmail.com>
2024-06-30 17:29:16 +0800

    version 17.9.7

---
+++ longest common prefix/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class Program
+{
+    static void Main()
+    {
+        string[] strs = { "flower", "flow", "flight" };
+        Console.WriteLine("Longest Common Prefix: " + LongestCommonPrefix(strs));
+    }
+
+    public static string LongestCommonPrefix(string[] strs)
+    {
+        if (strs == null || strs.Length == 0)
+        {
+            return string.Empty;
+        }
+
+        string prefix = strs[0];
+        for (int i = 1; i < strs.Length; i++)
+        {
+            while (strs[i].IndexOf(prefix) != 0)
+            {
+                prefix = prefix.Substring(0, prefix.Length - 1);
+                if (string.IsNullOrEmpty(prefix))
+                {
+                    return string.Empty;
+                }
+            }
+        }
+        return prefix;
+    }
+}
